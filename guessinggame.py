d205b06c041ec6cc0c644645a7bb2f74797f0f41
Mitzi Song <mitzisong@mitzi-songs-macbook.local>
2014-02-09 21:20:38 -0800

    Completed exercise

--- guessinggame.py
+++ guessinggame.py
@@ -1,12 +1,22 @@
 from random import randint 
-tries = 0
 
-print "Howdy, what's your name?", 
-name = raw_input()
+def game():
+    name = raw_input("Howdy, what's your name?") 
+    print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
 
-print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
-print "Your guess?",
-guess = int(raw_input())
+    answer = randint(1, 100)
 
-answer = randint(1, 100)
-tries = 1
+    guess = 0
+    tries = 1
+
+    while guess != answer:
+        guess = int(raw_input('Your guess?'))
+        if guess < answer:
+            print "Your guess is too low, try again."
+            tries += 1
+        elif guess > answer:
+            print "Your guess is too high, try again."
+            tries += 1
+        else: print "Well done, %s! You found my number in %d tries." % (name, tries)
+
+game()
